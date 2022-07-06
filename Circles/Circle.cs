ec8a23459ae95b8b822e58e51fe7eeacbf496602
minjaeid990811 <gimm93838@gmail.com>
2022-07-06 18:19:31 +0900

    나머지

---
+++ Circles/Circle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+
+namespace Circles
+{
+    class Circle
+    {
+        private static Random rnd = new Random();
+        private double xcen, ycen, radius;
+        private int red, green, blue;
+        private double xmov, ymov;
+
+        // 생성자 - 주어진 위치에 랜덤 크기, 랜덤 색의 원 생성
+        public Circle(double xcen, double ycen)
+        {
+            this.xcen = xcen;
+            this.ycen = ycen;
+            this.radius = rnd.Next(10) + 2;
+            this.red = rnd.Next(200);
+            this.green = rnd.Next(200);
+            this.blue = rnd.Next(200);  // 너무밝지않게
+            while (true) { this.xmov = rnd.Next(-3, 3); if (xmov != 0) break; }
+            while (true) { this.ymov = rnd.Next(-3, 3); if (ymov != 0) break; }
+        }
+
+        // 움직임
+        public void move(double xmin, double xmax, double ymin, double ymax)
+        {
+            if (xcen <= xmin + radius) xmov = Math.Abs(xmov);
+            if (xcen >= xmax - radius) xmov = -Math.Abs(xmov);
+            if (ycen <= ymin + radius) ymov = Math.Abs(ymov);
+            if (ycen >= ymax - radius) ymov = -Math.Abs(ymov);
+            xcen += xmov;
+            ycen += ymov;
+        }
+
+        // 그리기
+        public void draw(TCanvas canvas)
+        {
+            canvas.DrawEllipse(Color.FromArgb(this.red, this.green, this.blue),
+            this.xcen, this.ycen, this.radius * 2, this.radius * 2);
+        }
+
+        // 주어진 점이 내부인지 판단
+        public bool isinside(double xp, double yp)
+        {
+            double dx = xp - this.xcen;
+            double dy = yp - this.ycen;
+            double dist = Math.Sqrt(dx * dx + dy * dy);
+            return (dist <= this.radius) ? true : false;
+        }
+        // 원이 원 내부인지 판단
+        /*public bool isCINC(double xp, double yp,double r)
+        {
+            // 반지름차이 > 중심거리 일 경우 작은원이 큰 원 안에 들어감.
+            double dr = Math.Abs(r-this.radius); // 반지름차이
+            double dx = Math.Abs(xp - this.xcen); 
+            double dy = Math.Abs(yp - this.ycen);
+            double dist = Math.Sqrt(dx * dx + dy * dy);// 중심거리
+            return (dr > dist) ? true : false; //반지름차이 > 중심거리인가?
+        }*/
+        public bool isCINC(Circle cir)
+        {
+            // 반지름차이 > 중심거리 일 경우 작은원이 큰 원 안에 들어감.
+            double dr = cir.radius - this.radius; // 반지름차이
+            double dx = Math.Abs(cir.xcen - this.xcen);
+            double dy = Math.Abs(cir.ycen - this.ycen);
+            double dist = Math.Sqrt(dx * dx + dy * dy);// 중심거리
+            return (dr > dist) ? true : false; //반지름차이 > 중심거리인가?
+        }
+        
+    }
+}
---
+++ Circles/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circles
+{
+    public partial class Form1 : Form
+    {
+        private TCanvas canvas = null;
+        private List<Circle> circles = new List<Circle>();
+        private Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            if (canvas == null) canvas = new TCanvas(picDraw, 0, 100, 0, 100);
+        }
+
+        private void DrawCircles()
+        {
+            // 화면지우기
+            canvas.ClearDrawing(Color.White);
+            // 모든 원 그리기
+            for (int i = 0; i < circles.Count; i++)
+            {
+                circles[i].draw(canvas);
+            }
+            lblNCir.Text = "원의개수 : " + Convert.ToString(circles.Count);
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+            //움직임
+            if (chkMove.Checked == false) return; 
+            for (int i = 0; i < circles.Count; i++)
+            {
+                circles[i].move(0, 100, 0, 100);
+            }
+            DrawCircles();
+
+            //원안에 원이 들어가면 작은원 지우기
+
+            if (chkDelIncluded.Checked)
+            {
+                List<Circle> cdel = new List<Circle>();
+                for (int i = 0; i < circles.Count; i++)
+                {
+                    for (int j = 0; j < circles.Count; j++)
+                    {
+                        if (circles[i].isCINC(circles[j]))
+                            cdel.Add(circles[i]);
+                    }
+                }
+                for (int i = 0; i < cdel.Count; i++)
+                    circles.Remove(cdel[i]);
+            }
+
+
+        }
+          
+        
+        private void picDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (radAdd.Checked)
+            {
+                Circle cir = new Circle(canvas.xposD(e.X), canvas.yposD(e.Y));
+                circles.Add(cir);
+                DrawCircles();
+            }
+            else if (radDel.Checked)
+            {
+                List<Circle> cdel = new List<Circle>();
+                for (int i = 0; i < circles.Count; i++)
+                {
+                    if (circles[i].isinside(canvas.xposD(e.X), canvas.yposD(e.Y))) cdel.Add(circles[i]);
+                }
+
+                for (int i = 0; i < cdel.Count; i++) circles.Remove(cdel[i]);
+                DrawCircles();
+            }
+        }
+        private void btnAdd100_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                double xc = rnd.Next(100);
+                double yc = rnd.Next(100);
+                Circle cir = new Circle(xc, yc);
+                circles.Add(cir);
+            }
+            DrawCircles();
+        }
+        private void btnDelAll_Click(object sender, EventArgs e)
+        {
+            circles.Clear();
+            DrawCircles();
+        }
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
